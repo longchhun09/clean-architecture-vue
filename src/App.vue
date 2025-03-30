09f05ddc67a325daf0826fba04ae89a17b1367c1
longchhun <mark.chhunlong@gmail.com>
2025-03-30 16:55:59 +0700

    Initialize Vue 3 project with Vite, including essential configuration files, dependencies, and a basic app structure. Added ESLint, Prettier, and TypeScript support, along with a README for project setup instructions.

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import TodoList from './presentation/TodoList.vue';
+</script>
+
+<template>
+  <div class="app">
+    <TodoList />
+  </div>
+</template>
+
+<style>
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #f0f2f5;
+}
+</style>
